6279f4dc07423607e4cce3ba1f4b3f699130338b
Vladimir Umek <vladimir.umek@arm.com>
2026-08-27 08:57:14 +0200

    FileSystem: corrected FAT name cache entry size from 48 to 40 bytes

--- Components/FileSystem/Config/FS_Config_NAND.h
+++ Components/FileSystem/Config/FS_Config_NAND.h
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------------------
  * MDK Middleware - Component ::File System:Drive
- * Copyright (c) 2004-2024 Arm Limited (or its affiliates). All rights reserved.
+ * Copyright (c) 2004-2026 Arm Limited (or its affiliates). All rights reserved.
  *------------------------------------------------------------------------------
  * Name:    FS_Config_NAND_%Instance%.h
  * Purpose: File System Configuration for NAND Flash Drive
@@ -142,7 +142,7 @@
 //   </e>
 //   <o>Filename Cache Size <0-1000000>
 //   <i>Define number of cached file or directory names.
-//   <i>48 bytes of RAM is required for each cached name.
+//   <i>40 bytes of RAM is required for each cached name.
 #define NAND%Instance%_NAME_CACHE_SIZE   0
 
 //   <q>Use FAT Journal
